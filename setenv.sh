13bf72a40b259a041b6ce80d4cb4e0af0dd337df
Andre Fernandes <andre@vertigo.com.br>
2019-08-20 09:37:11 -0300

    profile fixes

--- setenv.sh
+++ setenv.sh
@@ -2,9 +2,12 @@
 #
 # CAS_SERVER must point to a cluster instance
 #
-export CAS_SERVER=pecus.serveo.net
+export CAS_SERVER=coepi.serveo.net
 export CAS_DOMAIN=serveo.net
 export ALA_URL=https://$CAS_SERVER # traefik port
+#export CAS_SERVER=54.166.197.103
+#export CAS_DOMAIN=54.166.197.103
+#export ALA_URL=http://54.166.197.103 # traefik port
 sed "s#\$ALA_URL#$ALA_URL#g" ./config/application-template.yml | \
  sed "s#\$CAS_DOMAIN#$CAS_DOMAIN#g" | \
  sed "s#\$CAS_SERVER#$CAS_SERVER#g" > ./config/application.yml
